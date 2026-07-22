a50beabec51d4c4405396bd22142164d73ed3acc
Filip013 <filip013@gmail.com>
2026-07-22 12:51:09 +0200

    feat: add VS Code launch command (code .) to z_agy.bat script

--- z_agy.bat
+++ z_agy.bat
@@ -1,5 +1,7 @@
 @echo off
 title Antigravity CLI Launcher
 cd /d "%~dp0"
-echo Launching Antigravity CLI (agy -c) in: %CD%
+echo Opening VS Code in: %CD%
+start "" code .
+echo Launching Antigravity CLI (agy -c)...
 powershell -NoExit -Command "agy -c"
